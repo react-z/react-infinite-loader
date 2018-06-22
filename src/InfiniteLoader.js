274a3f4b12b82bd0d36241ffaf55481d8f9351c2
Andrew Wilson <andrew@lightsinthesky.io>
2018-06-22 09:27:27 -0300

    react 16 and proptypes update

--- src/InfiniteLoader.js
+++ src/InfiniteLoader.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react'
-import Visit from './Visit'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Visit from "./Visit";
 
 export default class InfiniteLoader extends Component {
+  static get defaultProps() {
+    return {
+      visitStyle: { visibility: "hidden" }
+    };
+  }
 
-  static get defaultProps () {
-     return {
-       visitStyle: { visibility: 'hidden' }
-     }
-   }
-
-   static get propTypes () {
-     return {
-       containerElement: React.PropTypes.object,
-       onVisited: React.PropTypes.func,
-       visitStyle: React.PropTypes.object,
-       loaderStyle: React.PropTypes.object
-     }
+  static get propTypes() {
+    return {
+      containerElement: PropTypes.object,
+      onVisited: PropTypes.func,
+      visitStyle: PropTypes.object,
+      loaderStyle: PropTypes.object
+    };
   }
 
   constructor(props) {
@@ -23,20 +23,18 @@ export default class InfiniteLoader extends Component {
   }
 
   handleVisit() {
-    this.refs.visit.resetVisited()
-    if(this.props.onVisited != undefined){
-      this.props.onVisited()
+    this.refs.visit.resetVisited();
+    if (this.props.onVisited != undefined) {
+      this.props.onVisited();
     }
   }
 
   render() {
-
     return (
       <span>
-        <div className='loader' style={this.props.loaderStyle} />
-        <Visit className='visit' ref='visit'
-               {...this.props} onVisited={ () => this.handleVisit() } />
+        <div className="loader" style={this.props.loaderStyle} />
+        <Visit className="visit" ref="visit" {...this.props} onVisited={() => this.handleVisit()} />
       </span>
-    )
+    );
   }
 }
